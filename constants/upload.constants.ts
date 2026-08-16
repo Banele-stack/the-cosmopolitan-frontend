59c4b102302bdb7960058a775c6c14c36dcf7f68
Banele Ngubane <84304786+Banele-stack@users.noreply.github.com>
2026-08-16 13:03:30 +0200

    Delete constants directory

--- constants/upload.constants.ts
+++
@@ -1,6 +0,0 @@
-export const MAX_FILE_SIZE = 5 * 1024 * 1024;
-export const ALLOWED_IMAGE_TYPES = ["image/jpeg", "image/png", "image/webp", "image/gif"];
-
-export const MAX_VIDEO_SIZE = 50 * 1024 * 1024;
-export const ALLOWED_VIDEO_TYPES = ["video/mp4", "video/quicktime", "video/webm", "video/x-msvideo", "video/3gpp"];
-export const MAX_VIDEOS = 3;
